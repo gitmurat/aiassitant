dd9e1796593220da0382d65dd73f459ff5a9ab29
fertflabs <fernando.escobar@thefringelabs.com>
2025-02-03 14:00:59 +0100

    Change page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,10 @@ import { assistantId } from "./assistant-config";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Assistants API Quickstart",
-  description: "A quickstart template using the Assistants API with OpenAI",
+  title: "AFFLUX assistant",
+  description: "",
   icons: {
-    icon: "/openai.svg",
+    icon: "/afflux-favicon.png",
   },
 };
 
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {assistantId ? children : <Warnings />}
-        <img className="logo" src="/openai.svg" alt="OpenAI Logo" />
+        {/* <img className="logo" src="/afflux-logo-blue.png" alt="OpenAI Logo" /> */}
       </body>
     </html>
   );
