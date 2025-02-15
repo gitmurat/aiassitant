7be38066aa49e521e644cddd0b8d8291ba413bc0
Kazuhiro Sera <seratch@gmail.com>
2025-02-15 23:18:03 +0900

    Fix #37 by upgrading Next.js to 15.x

--- app/api/assistants/threads/[threadId]/messages/route.ts
+++ app/api/assistants/threads/[threadId]/messages/route.ts
@@ -4,7 +4,8 @@ import { openai } from "@/app/openai";
 export const runtime = "nodejs";
 
 // Send a new message to a thread
-export async function POST(request, { params: { threadId } }) {
+export async function POST(request, { params }) {
+  const { threadId } = await params;
   const { content } = await request.json();
 
   await openai.beta.threads.messages.create(threadId, {
